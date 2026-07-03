bee7ab48c51d831960393ed70fff84c26db13684
Alla Tsaiukova <front.dev.edu@gmail.com>
2026-07-03 19:22:43 +0200

    fix: ignore clickaway on close

--- src/providers/toast-provider/Toast.tsx
+++ src/providers/toast-provider/Toast.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Alert, Grow, GrowProps, Snackbar } from '@mui/material';
+import { Alert, Grow, GrowProps, Snackbar, SnackbarCloseReason } from '@mui/material';
 
 import { ToastType } from './types';
 
@@ -12,15 +12,19 @@ type ToastProps = {
 };
 
 export function Toast({ message, onClose, open, severity }: ToastProps) {
+  const handleClose = (_event: Event | React.SyntheticEvent, reason?: SnackbarCloseReason) => {
+    if (reason !== 'clickaway') onClose();
+  };
+
   return (
     <Snackbar
       anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       autoHideDuration={5000}
-      onClose={onClose}
+      onClose={handleClose}
       open={open}
       slots={{ transition: GrowTransition }}
     >
-      <Alert onClose={onClose} severity={severity} variant="filled">
+      <Alert onClose={handleClose} severity={severity} variant="filled">
         {message}
       </Alert>
     </Snackbar>
