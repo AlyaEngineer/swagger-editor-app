1856f8cc62f19e7f5be78eb00889cc697d29a533
Alla Tsaiukova <front.dev.edu@gmail.com>
2026-07-07 00:46:50 +0200

    refactor: extract DEFAULT_SCHEMA into a constant

--- src/utils/hooks/swagger-hook.ts
+++ src/utils/hooks/swagger-hook.ts
@@ -1,33 +1,9 @@
 import { useEffect, useState } from 'react';
 
+import { DEFAULT_SCHEMA } from '@/constants/default-schema';
 import { OpenApiDocument } from '@/types';
 import { convertSchema, type SchemaFormat } from '@/utils/swagger-editor/schema-format';
 import { validateSwaggerSchema } from '@/utils/swagger-editor/swagger-validation';
-
-const DEFAULT_SCHEMA = `openapi: 3.0.0
-info:
-  title: Swagger Editor App
-  version: 1.0.0
-paths:
-  /users:
-    get:
-      summary: Get users
-      responses:
-        '200':
-          description: Successful response
-  /users/{id}:
-    get:
-      summary: Get user by id
-      parameters:
-        - name: id
-          in: path
-          required: true
-          schema:
-            type: string
-      responses:
-        '200':
-          description: Successful response
-`;
 
 export const useSwaggerHook = () => {
   const [editorValue, setEditorValue] = useState(DEFAULT_SCHEMA);
