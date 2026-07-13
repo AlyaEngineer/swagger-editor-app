f9d25a4ffee9a2646f7506e992792307f055e799
Alla Tsaiukova <front.dev.edu@gmail.com>
2026-07-14 00:39:03 +0200

    refactor: update tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -29,6 +29,8 @@ export default defineConfig({
     },
     environment: 'jsdom',
     globals: true,
+    hookTimeout: 50_000,
     setupFiles: ['./vitest.setup.ts'],
+    testTimeout: 50_000,
   },
 });
